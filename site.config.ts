6011d7bde254c725ad7586817398d9337ad64776
siyuansc <113861839+siyuansc@users.noreply.github.com>
2023-03-06 19:08:24 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '064603b9235642ed9a42e86c1d4af186',
+  rootNotionPageId: '6a62dfec326848209f67fccfccacdef1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'My Personal Blog',
+  domain: 'https://grass-cesium-5b1.notion.site/Personal-Blog-6a62dfec326848209f67fccfccacdef1',
+  author: 'Siyuan Cao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
